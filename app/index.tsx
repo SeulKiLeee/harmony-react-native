286c40262d05fa542d757455f5a3b35e201af464
Funncy <gywns41@naver.com>
2021-10-09 11:24:50 +0900

    Implement : Main.tsx structure & routes.ts structure

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+
+const index = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Hello </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default index;
---
+++ app/pages/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+
+const MainPage = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Hello </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+export default MainPage;
